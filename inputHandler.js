53e6754556541c3c2eb664884633e933a0e39046
1JeffWho <94126816+1JeffWho@users.noreply.github.com>
2023-05-05 18:03:18 +0200

    v1.5.1

--- inputHandler.js
+++ inputHandler.js
@@ -15,18 +15,49 @@ document.getElementById("faster").onclick = function(){
     }
     }
 
-addEventListener("keydown", function(e){
+document.addEventListener("keydown", function(e){
     if (e.code == "KeyD") vxr = vxr_value;
     if (e.code == "KeyA") vxl = vxl_value;
     if (e.code == "KeyS") vy = vy_value;
     if (e.code == "KeyW") vy = vy_minus_value;
-
-})
-
-addEventListener("keyup", function(e){
+    });
+    
+    document.addEventListener("keyup", function(e){
     if (e.code == "KeyD") vxr = 0;
     if (e.code == "KeyA") vxl = 0;
     if (e.code == "KeyS") vy = 0;
     if (e.code == "KeyW") vy = 0;
-})
-
+    });
+    
+    // Get the buttons for each key
+    const wButton = document.getElementById('w');
+    const aButton = document.getElementById('a');
+    const sButton = document.getElementById('s');
+    const dButton = document.getElementById('d');
+    
+    // Add a click event listener to the wButton
+    wButton.addEventListener('mousedown', () => {
+        vy = vy_minus_value;
+    });
+    wButton.addEventListener('mouseup', () => {
+        vy = 0;
+    });
+    sButton.addEventListener('mousedown', () => {
+        vy = vy_value;
+    });
+    sButton.addEventListener('mouseup', () => {
+        vy = 0;
+    });
+    aButton.addEventListener('mousedown', () => {
+        vxl = vxl_value;
+    });
+    aButton.addEventListener('mouseup', () => {
+        vxl = 0;
+    });
+    dButton.addEventListener('mousedown', () => {
+        vxr = vxr_value;
+    });
+    dButton.addEventListener('mouseup', () => {
+        vxr = 0;
+    });
+            
